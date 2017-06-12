1c5e912257f2e4eb6d47f264147fea0bebd3130b
Nicolas Ardison <VSINICAR@visa2.com.ar>
2017-06-11 22:18:08 -0300

    Firs rev. of the mvc example 3

---
+++ MVC_Example3/src/main/java/hello/GreetingController.java
@@ -0,0 +1,21 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Map;
+import java.util.HashMap;
+
+@Controller
+public class GreetingController {
+    
+    @RequestMapping("/home")
+    public String home(){ 
+    	return "greeting";
+    }
+    
+}
